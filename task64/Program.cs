c3a5154dcec3c41e57506397b9efdb9c6975065e
Alexander Zhuravlev <warcueid@mail.ru>
2024-04-12 18:43:50 +0400

    Done task64

--- task64/Program.cs
+++ task64/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Task conditions: N = 5 -> 5, 4, 3, 2, 1 and N = 8 -> 8, 7, 6, 5, 4, 3, 2, 1");
+int Prompt(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+int NaturalNumber(int n, int m)
+{
+  if (n == m) return n;
+  else Console.Write($"{NaturalNumber(n, m + 1)},");
+  return m;
+}
+int n = Prompt("Еnter only natural numbers  N: ");
+Console.WriteLine(NaturalNumber(n, 1));
